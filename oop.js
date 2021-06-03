faa6858d13abc6df6025f49c648113a8bde09ba6
aniledev <elinamcgill@gmail.com>
2021-06-03 03:38:58 -0400

    FEAT: create methods for converting rgb values to hsl values

--- oop.js
+++ oop.js
@@ -80,27 +80,77 @@ class ColorClass {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
   }
   shortRGB() {
     const { r, g, b } = this;
     return `${r}, ${g}, ${b}`;
   }
+
   rgb() {
     const { r, g, b } = this;
     return `rgb(${this.shortRGB()})`;
   }
+
   rgba(a = 1.0) {
     const { r, g, b } = this;
     return `rgba(${this.shortRGB()}, ${a})`;
   }
+
   hex() {
     const { r, g, b } = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
+
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l})%`;
+  }
+  calcHSL() {
+    let { r, g, b } = this;
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+
+    h == Math.round(h * 60);
+
+    // Make negative hues positive behind
+    if (h < 0) h += 360;
+    //Calculate lightness
+    l = (cmax - cmin) / 2;
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
 }
 
 const c1 = new ColorClass(255, 67, 89, "tomato");
+const white = new ColorClass(255, 255, 255, "white");
 // console.log(c1);
 // console.log(c1.rgb());
 // console.log(c1.rgba());
 // console.log("hex", c1.hex());
+console.log(white.hsl());
